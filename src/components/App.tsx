93e0f51b61298bd3bac6e3f736dae6413b50a265
Thanapat Pirmphol <t.pirmphol@gmail.com>
2022-07-16 00:47:29 +0700

    no message

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -22,7 +22,7 @@ const App: React.FunctionComponent = () => {
                 htmlFor="accountId"
                 className="block text-sm font-medium text-gray-700"
               >
-                Account Id
+                Account ID
               </label>
               <div className="mt-1">
                 <input
@@ -30,6 +30,7 @@ const App: React.FunctionComponent = () => {
                   name="accountId"
                   type="text"
                   required
+                  placeholder="0.0.xxxx"
                   className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
                 />
               </div>
@@ -50,6 +51,23 @@ const App: React.FunctionComponent = () => {
                   required
                   className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
                 />
+              </div>
+            </div>
+
+            <div className="flex items-center justify-between">
+              <div className="flex items-center">
+                <input
+                  id="remember-me"
+                  name="remember-me"
+                  type="checkbox"
+                  className="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded"
+                />
+                <label
+                  htmlFor="remember-me"
+                  className="ml-2 block text-sm text-gray-900"
+                >
+                  Testnet Account
+                </label>
               </div>
             </div>
 
